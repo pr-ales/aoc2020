346348f05d4ac2b0d5a8051b5ee016a6312af414
Aleš Podolník <ales@podolnik.me>
2020-12-11 12:18:24 +0100

    Day 11 polishing

--- 11/day11.py
+++ 11/day11.py
@@ -19,23 +19,27 @@ with open('input.txt', mode='r') as f:
 def new_state(data):
     return np.full(np.shape(data), np.nan)
 
-def flip(data, row, col):
-    r_max = len(data)
-    c_max = len(data[0])
-    seats = data[max(row-1,0):min(row+2, r_max),max(0, col-1):min(col+2, c_max)] == 1
+def flip(state, row, col, condition_n, check_seats):
+    r_max = len(state)
+    c_max = len(state[0])
+       
+    filtered_seats = [(s[0] + row, s[1] + col) for s in check_seats if s[1] + col >= 0 and s[0] + row >= 0 and s[1] + col < c_max and s[0] + row < r_max]
+    
+    r = [s[0] for s in filtered_seats]
+    c = [s[1] for s in filtered_seats]
+    
+    seats = state[r,c] == 1
     total = seats.sum()
     
-    seat = data[row,col]
-    
-    occupied = data[row,col] == 1
-    empty = data[row,col] == 0
+    occupied = state[row,col] == 1
+    empty = state[row,col] == 0
     
     if empty and total == 0:
         return 1
-    if occupied and total - 1 >= 4:
+    if occupied and total >= condition_n:
         return 0
     
-    return data[row, col]
+    return state[row, col]
 
 def show(data):
     for l in data:
@@ -45,20 +49,30 @@ def show(data):
 # show(data)
 
 # part 1
+    
+directions = [-1, 0, 1]
+
+default_closest = []
+for d_r in directions:
+    for d_c in directions:
+        if d_r == 0 and d_c == 0:
+            pass
+        else:
+            default_closest.append((d_r, d_c))
 
 state = np.array(data)
-
+        
 while True:
     next_state = new_state(state)    
     for r in range(len(state)):
         for c in range(len(state[0])):
-            next_state[r,c] = flip(state, r, c)        
+            next_state[r,c] = flip(state, r, c, 4, default_closest)        
    
     if np.array_equal(next_state, state):
         break
     
     state = next_state
-
+    
 occupied = state == 1
 print(occupied.sum())
 
@@ -68,8 +82,6 @@ closest = [[[] for i in range(len(l))] for l in data]
 
 r_max = len(data)
 c_max = len(data[0])
-
-directions = [-1, 0, 1]
 
 for row in range(r_max):
     for col in range(c_max):
@@ -88,36 +100,14 @@ for row in range(r_max):
                             r = r + d_r
                             c = c + d_c
 
-def flip2(data, row, col):
-    
-    check_seats = closest[row][col]
-    
-    r = [s[0] + row for s in check_seats]
-    c = [s[1] + col for s in check_seats]
-    
-    seats = data[r,c] == 1
-    total = seats.sum()
-    
-    seat = data[row,col]
-    
-    occupied = data[row,col] == 1
-    empty = data[row,col] == 0
-    
-    if empty and total == 0:
-        return 1
-    if occupied and total >= 5:
-        return 0
-    
-    return data[row, col]
-
-
 state = np.array(data)
 
 while True:
     next_state = new_state(state)    
     for r in range(len(state)):
         for c in range(len(state[0])):
-            next_state[r,c] = flip2(state, r, c)        
+            check_seats = closest[r][c]
+            next_state[r,c] = flip(state, r, c, 5, check_seats)        
    
     if np.array_equal(next_state, state):
         break
