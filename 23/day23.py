550b6b598414fc7437576189b979cda1f6457806
Aleš Podolník <ales@podolnik.me>
2020-12-23 15:22:43 +0100

    Day 23 solution (faster)

--- 23/day23.py
+++ 23/day23.py
@@ -8,63 +8,124 @@ Created on Wed Dec 23 08:28:12 2020
 
 from time import time
 
-input_text = '389125467' # test input
+#input_text = '389125467' # test input
 input_text = '253149867'
 
-cups = [int(c) for c in input_text]
-
-def shuffle(cups, n_moves = 100):
+class Cup:
+    def __init__(self, val, next_cup = None):
+        self.val = int(val)
+        self.next_cup = next_cup
+  
+    def get_data(self, count = -1):
+        data = [self.val]
+        next_cup = self.next_cup
+        i = 0
+        while next_cup != self and next_cup is not None:
+            data.append(next_cup.val)
+            next_cup = next_cup.next_cup
+            i += 1
+            
+            if count > 0 and i >= count:
+                break
+            
+        return data
     
-    current = cups[0]
-    n_cups = len(cups)
-
-    for m in range(n_moves):
-#        print(cups)
-        i_c = cups.index(current)
-        picked = []    
-        i_adj = (i_c + 1) % n_cups
-        for _ in range(3):
-            if i_adj >= len(cups):
-                i_adj = 0
-            picked.append(cups.pop(i_adj))
+    def show(self):
+        data = self.get_data()
+        print(', '.join([str(v) for v in data]))
         
-#        print(current, picked)
+    @staticmethod
+    def get_chain(input_text, million = False):
+        arr = list(input_text)
+        helper = {}
         
-        next_cup = current - 1 if current > 1 else max(cups)
-        while next_cup in picked:
-            next_cup = next_cup - 1 if next_cup > 1 else max(cups)
+        first_cup = Cup(arr[0])
+        prev_cup = first_cup
         
-#        print(next_cup)
+        helper[first_cup.val] = first_cup
         
-        i_n = cups.index(next_cup)
+        for i in range(1, len(arr)):
+            next_cup = Cup(arr[i])
+            helper[next_cup.val] = next_cup
+            
+            prev_cup.next_cup = next_cup
+            prev_cup = next_cup
         
-        for i in range(3):
-            cups.insert(i_n + 1 + i, picked[i])
+        if million:
+            prev_cup
+            for i in range(len(arr) + 1, 1000001):
+                next_cup = Cup(i)
+                helper[next_cup.val] = next_cup
+                
+                prev_cup.next_cup = next_cup
+                prev_cup = next_cup
+                        
+        prev_cup.next_cup = first_cup
         
-        current = cups[(cups.index(current) + 1) % n_cups]
+        return first_cup, helper
+    
+    def cut(self, n):
+        cut = self.next_cup
+        seam = self.next_cup
+        for i in range(n - 1):
+            seam = seam.next_cup
         
-        if m % 1000 == 0:
-            print(m)
-    return cups
+        self.next_cup = seam.next_cup
+        seam.next_cup = None
+        return cut
+    
+    def insert(self, cup):
+        last = cup
+        while last.next_cup is not None:
+            last = last.next_cup
+            
+        last.next_cup = self.next_cup
+        self.next_cup = cup
+    
+    def shuffle(self, helper, n_iter = 10):
+        max_val = max(helper.keys())
+        
+        pivot = self
+        
+        for i in range(n_iter):
+                       
+            picked = pivot.cut(3)
+            dest_val = max_val if pivot.val == 1 else pivot.val - 1
+            
+            while picked.contains(dest_val):
+                dest_val = max_val if dest_val == 1 else dest_val - 1
+            
+            dest_cup = helper[dest_val]
+            dest_cup.insert(picked)
+            
+            pivot = pivot.next_cup
+        
+    def contains(self, val):
+        cup = self
+        while True:
+            if cup.val == val:
+                return True
+            cup = cup.next_cup
+            if cup is None or cup == self:
+                return False
+        
+        
+start_1 = time()
+chain, helper = Cup.get_chain(input_text)
 
-cups = shuffle(cups)
+chain.shuffle(helper, n_iter = 100)
+data = helper[1].get_data()
+end_1 = time()
 
-print('final')
+print(''.join([str(c) for c in data[1:]]))
+print(end_1 - start_1)
 
-i_1 = cups.index(1)
-ordering = [cups[(i_1 + i + 1) % len(cups)] for i in range(len(cups) - 1)]
+start_2 = time()
+chain, helper = Cup.get_chain(input_text, million = True)
 
-print(''.join([str(c) for c in ordering]))
-
-cups = [int(c) for c in input_text]
-
-rest = [i for i in range(max(cups) + 1, 1000001)]
-cups.extend(rest)
-
-cups = shuffle(cups, n_moves = 1000000)
-
-i_1 = cups.index(1)
-stars = [cups[(i_1 + i + 1) % len(cups)] for i in range(2)]
-print(stars, stars[0] * stars[1])
-
+chain.shuffle(helper, n_iter = 10000000)
+data = helper[1].get_data(count=2)
+end_2 = time()
+print(data[1] * data[2])
+print(end_2 - start_2)
 
